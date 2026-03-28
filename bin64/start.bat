fe60d7942d7971e2e0649e5bbd35d9f7957204d9
oofbomb <okjustrandomemail@gmail.com>
2026-03-28 12:15:19 -0500

    90

--- bin64/start.bat
+++ bin64/start.bat
@@ -1,9 +1,36 @@
 @echo off
-echo Starting 3proxy...
 cd "C:\Users\mituy\Downloads\3proxy-modified\bin64"
-start "" cmd /k "3proxy.exe 3proxy.cfg"
-echo Starting Node.js server...
-cd "C:\Users\mituy\Downloads\3proxy-modified\bin64"
-start "" cmd /k "nodemon server.js"
-echo All services started.
+
+:MENU
+echo Select mode:
+echo 1 - Only 3Proxy
+echo 2 - 3Proxy + Node.js server
+echo 3 - Exit
+set /p MODE=Enter mode number (1, 2, or 3): 
+
+if "%MODE%"=="1" (
+    echo Starting 3proxy...
+    start "" cmd /k "3proxy.exe 3proxy.cfg"
+    goto END
+)
+
+if "%MODE%"=="2" (
+    echo Starting 3proxy...
+    start "" cmd /k "3proxy.exe 3proxy.cfg"
+    echo Starting Node.js server...
+    start "" cmd /k "nodemon server.js"
+    goto END
+)
+
+if "%MODE%"=="3" (
+    echo Exiting...
+    goto END
+)
+
+echo Invalid input. Please enter 1, 2, or 3.
+echo.
+goto MENU
+
+:END
+echo All requested actions completed.
 pause
