81d9cab90ee5a3f42597abbae829906d353e6214
oofbomb <okjustrandomemail@gmail.com>
2026-03-28 17:06:56 -0500

    8

--- bin64/server.js
+++ bin64/server.js
@@ -7,7 +7,7 @@ const http     = require('http');
 const net      = require('net');
 const readline = require('readline');
 const app      = express();
-const PORT     = 8080;
+const PORT     = 80;
 
 // ─── AUTH ────────────────────────────────────────────────────────────────────
 const USER  = 'oofbomb';
